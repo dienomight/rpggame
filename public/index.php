aac467b8619e0af07359ddfb61ef30c1cbb95518
DieNoMight <dienomight@gmail.com>
2019-01-05 17:51:58 +0100

    setting rules for apache access (.htaccess) + basic routing in index.php

--- public/index.php
+++ public/index.php
@@ -4,5 +4,24 @@ namespace rpggame;
 
 require_once '../src/AppInit.php';
 
+// setting up constants, autoloading, starting session & ob
 AppInit::run();
 
+// ROUTING
+$req = $_SERVER['REQUEST_URI'];
+$qs = $_SERVER['QUERY_STRING'];
+
+if (!empty($qs)) {
+    $req = substr($req, 0, strpos($req, '?'));
+}
+$req = str_replace(WWW_ROOT, '', $req);
+echo "request after trim: " . $req . BR;
+
+switch ($req) {
+    case('/login'):
+        echo "This is login page";
+        break;
+    default:
+        echo "This is default page";
+        break;
+}
